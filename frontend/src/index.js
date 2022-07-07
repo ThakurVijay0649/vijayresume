24b534ec7f78a4131cd747103c37d574eb7eadd0
Vijay <thakurvijay0006@gmail.com>
2022-07-07 10:42:07 +0530

    Fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import { Provider } from "react-redux";
-import store from "./Store";
-import { Provider as AlertProvider, positions, transitions } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
+import { Provider as AlertProvider, positions, transitions } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+import store from './Store';
 
 const options = {
-  position: positions.BOTTOM_CENTER,
-  timeout: 5000,
-  transition: transitions.SCALE,
-};
+    position: positions.BOTTOM_CENTER,
+    timeout: 5000,
+    transition: transitions.SCALE
+}
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <Provider store={store}>
-      <AlertProvider template={AlertTemplate} {...options}>
-        <App />
-      </AlertProvider>
+     <AlertProvider template={AlertTemplate} {...options} >
+      <App />
+     </AlertProvider>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
 );
